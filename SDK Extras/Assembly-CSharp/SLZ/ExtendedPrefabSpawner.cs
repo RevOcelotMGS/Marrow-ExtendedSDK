f480fcbb44871c7bb20c6d6da773941f4ef875aa
Snake8Bit <snake1byte@outlook.com>
2023-12-29 05:55:00 +0100

    Fixed namespace for ExtendedPrefabSpawner Script

--- SDK Extras/Assembly-CSharp/SLZ/ExtendedPrefabSpawner.cs
+++ SDK Extras/Assembly-CSharp/SLZ/ExtendedPrefabSpawner.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using SLZ.Marrow.Data;
 using UnityEngine;
 
-namespace SLZ
+namespace SLZ.Utilities
 {
 	public class ExtendedPrefabSpawner : MonoBehaviour
 	{
